08242ce675b9fc4e2da277a23b4791aefb165c01
Mukul Mishra <mkmishra1895@gmail.com>
2017-10-27 10:47:57 +0530

    Adds ajax request logic

--- front-end/index.js
+++ front-end/index.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
 	$('#submit-query').on('click', function() {
-		$('body').html('<div class="container"> <button class="btn btn-primary new-search" onclick="reloadPage()">New Search</button> <div class="card"> <div class="card-body"> <blockquote class="blockquote mb-0"> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p> <h9 class="sub-text">Some text about header</h9> </blockquote> </div> </div> </div>');
+
+		$.ajax({
+			url: 'http://mukulmishra.me/',
+			// dataType: 'json',
+			// data: { },
+			success: function(data) {
+				data = {
+					href: 'http://example.com',
+					text: 'Machine learning Text',
+					subText: 'Machine learning course by MIT.'
+				};
+				
+				var finalString = '';
+				finalString += '<div class="container"> <button class="btn btn-primary new-search" onclick="reloadPage()">New Search</button> <div class="card"> <div class="card-body"> <blockquote class="blockquote mb-0"> <a  href="' + data.href +
+				  '"><p>' + data.text +
+				  '</p></a> <h9 class="sub-text">' + data.subText +
+				  '</h9> </blockquote> </div> </div> </div>';
+
+				$('body').html(finalString); 
+			},
+		});
 	});
 });
 
 function reloadPage() {
 	location.reload();
 }
-
